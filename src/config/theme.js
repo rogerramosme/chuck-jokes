9f190c776fbf0ad040003f12dce775311836f4e2
Roger Ramos <roger.paulo@easynvest.com.br>
2020-04-02 17:26:39 -0300

    Adds Styled Components and base theme config

---
+++ src/config/theme.js
@@ -0,0 +1,10 @@
+const Tokens = {
+  colors: {
+    yellow: '#fcd146',
+    blue: '#0057ff',
+    black: '#0057ff',
+    grey: '#4f4f4fZZZZ'
+  }
+}
+
+export default Tokens
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
+import theme from 'config/theme'
 import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
-  <React.StrictMode>Hello world</React.StrictMode>,
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>Hello World</ThemeProvider>
+  </React.StrictMode>,
   document.getElementById('root')
 )
 
