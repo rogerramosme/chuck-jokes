038a74e24943cb0a8996eba2758b62ac7b6a8e36
Roger Ramos <roger.paulo@easynvest.com.br>
2020-04-02 17:06:33 -0300

    Turning off comma and semi-comma (into the end of arrays)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>Hello world</React.StrictMode>,
   document.getElementById('root')
-);
+)
 
-serviceWorker.unregister();
+serviceWorker.unregister()
