8fde80f70238e3342b7c60d99adc318396d1dc6d
= <=>
2020-04-03 00:13:44 -0300

    Adds mocked store

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,5 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { categoriesRequest, categoriesRequestSuccess } from 'store/reducers/categories'
 import Content from 'components/Content'
+import Loading from 'components/Loading'
+import Error from 'components/Error'
 import {
   ChuckJokesLogo,
   CategoriesTitle,
@@ -8,38 +13,60 @@ import {
   Button
 } from './Categories.style'
 
+const mockCategories = [
+  'animal',
+  'career',
+  'celebrity',
+  'dev',
+  'explicit',
+  'fashion',
+  'food',
+  'history',
+  'money',
+  'movie',
+  'music',
+  'political',
+  'religion',
+  'science',
+  'sport',
+  'travel'
+]
+
 export default () => {
-  const categories = [
-    'animal',
-    'career',
-    'celebrity',
-    'dev',
-    'explicit',
-    'fashion',
-    'food',
-    'history',
-    'money',
-    'movie',
-    'music',
-    'political',
-    'religion',
-    'science',
-    'sport',
-    'travel'
-  ]
+  const { isFetching, isSuccess, isError, error, data } = useSelector(state => state.categories)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(categoriesRequest())
+    setTimeout(() => {
+      dispatch(categoriesRequestSuccess(mockCategories))
+    }, 2000)
+  }, [dispatch])
 
   return (
     <Content>
-      <ChuckJokesLogo />
-      <CategoriesTitle>Joke Categories</CategoriesTitle>
-      <CategoriesIntro>Select a category to start laughting. No pressure.</CategoriesIntro>
-      <CategoryList>
-        {categories.map(category => (
-          <Button key={category} to={`/category/${category}`}>
-            {category}
-          </Button>
-        ))}
-      </CategoryList>
+      {isFetching && <Loading />}
+      {!isFetching && isSuccess && (
+        <>
+          <ChuckJokesLogo />
+          <CategoriesTitle>Joke Categories</CategoriesTitle>
+          <CategoriesIntro>Select a category to start laughting. No pressure.</CategoriesIntro>
+          <CategoryList>
+            {data.map(category => (
+              <Link to={`/category/${category}`}>
+                <Button key={category}>{category}</Button>
+              </Link>
+            ))}
+          </CategoryList>
+        </>
+      )}
+      {isError && (
+        <Error
+          buttonText="Try again"
+          errorMessage={error}
+          handleError={() => console.log('handleError')}
+        />
+      )}
     </Content>
   )
 }
