5ae57340aa8ec7d28294805f237a2806718f6df2
= <=>
2020-04-03 00:13:18 -0300

    Adds history, ErrorPage and changes intro route

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,22 @@
 import React, { lazy, Suspense } from 'react'
-import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
+import Loading from 'components/Loading'
+import { createBrowserHistory } from 'history'
+import { Router, Switch, Route } from 'react-router-dom'
 
 const Intro = lazy(() => import('pages/Intro'))
 const Categories = lazy(() => import('pages/Categories'))
+const ErrorPage = lazy(() => import('components/Error'))
 
 export default () => (
-  <Router>
-    <Suspense fallback={<div>Loadding page...</div>}>
+  <Router history={createBrowserHistory()}>
+    <Suspense fallback={<Loading />}>
       <Switch>
-        <Route exact path="/intro" component={Intro} />
+        <Route exact path="/" component={Intro} />
         <Route exact path="/categories" component={Categories} />
-        <Redirect from="/" to="intro" />
+        <Route
+          path="*"
+          render={() => <ErrorPage errorMessage="The page you are looking for doesn't exist" />}
+        />
       </Switch>
     </Suspense>
   </Router>
