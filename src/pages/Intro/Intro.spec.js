3dd9d0dcb776950a955a22a1e91219d920f92c17
= <=>
2020-04-02 21:39:12 -0300

    Adding Router to Intro page test

--- src/pages/Intro/Intro.spec.js
+++ src/pages/Intro/Intro.spec.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import renderWithTheme from '__tests__/utils'
+import { BrowserRouter as Router } from 'react-router-dom'
 import Intro from './Intro'
 
 describe('Intro page', () => {
   it('Should render intro page', () => {
-    const { queryByText } = renderWithTheme(<Intro />)
+    const { queryByText } = renderWithTheme(
+      <Router>
+        <Intro />
+      </Router>
+    )
     expect(queryByText(/Chuck Jokes/)).toBeInTheDocument()
   })
 })
