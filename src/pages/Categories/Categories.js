d870a0fec787e7568e39812b3c5048e73ef9884e
Roger Ramos <rodgerpaulo@hotmail.com>
2020-04-04 16:28:54 -0300

    Add category API integration

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback, memo } from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux'
-import { categoriesRequest, categoriesRequestSuccess } from 'store/reducers/categories'
+import { categoriesRequest } from 'store/reducers/categories'
 import Content from 'components/Content'
 import Loading from 'components/Loading'
 import Error from 'components/Error'
@@ -13,60 +13,39 @@ import {
   Button
 } from './Categories.style'
 
-const mockCategories = [
-  'animal',
-  'career',
-  'celebrity',
-  'dev',
-  'explicit',
-  'fashion',
-  'food',
-  'history',
-  'money',
-  'movie',
-  'music',
-  'political',
-  'religion',
-  'science',
-  'sport',
-  'travel'
-]
-
-export default () => {
+export default memo(() => {
   const { isFetching, isSuccess, isError, error, data } = useSelector(state => state.categories)
   const dispatch = useDispatch()
 
+  const handleCategoriesRequest = useCallback(() => dispatch(categoriesRequest()), [dispatch])
+
   useEffect(() => {
-    dispatch(categoriesRequest())
-    setTimeout(() => {
-      dispatch(categoriesRequestSuccess(mockCategories))
-    }, 2000)
-  }, [dispatch])
+    handleCategoriesRequest()
+  }, [handleCategoriesRequest])
 
   return (
-    <Content>
-      {isFetching && <Loading />}
-      {!isFetching && isSuccess && (
-        <>
-          <ChuckJokesLogo />
-          <CategoriesTitle>Joke Categories</CategoriesTitle>
-          <CategoriesIntro>Select a category to start laughting. No pressure.</CategoriesIntro>
-          <CategoryList>
-            {data.map(category => (
-              <Link to={`/category/${category}`}>
-                <Button key={category}>{category}</Button>
-              </Link>
-            ))}
-          </CategoryList>
-        </>
+    <>
+      {!isError ? (
+        <Content>
+          {isFetching && <Loading />}
+          {!isFetching && isSuccess && (
+            <>
+              <ChuckJokesLogo />
+              <CategoriesTitle>Joke Categories</CategoriesTitle>
+              <CategoriesIntro>Select a category to start laughting. No pressure.</CategoriesIntro>
+              <CategoryList>
+                {data.map(category => (
+                  <Link key={category} to={`/category/${category}`}>
+                    <Button>{category}</Button>
+                  </Link>
+                ))}
+              </CategoryList>
+            </>
+          )}
+        </Content>
+      ) : (
+        <Error buttonText="Try again" errorMessage={error} handleError={handleCategoriesRequest} />
       )}
-      {isError && (
-        <Error
-          buttonText="Try again"
-          errorMessage={error}
-          handleError={() => console.log('handleError')}
-        />
-      )}
-    </Content>
+    </>
   )
-}
+})
