d877580d42cc28e30e40772a3f350c8bb7dd7f47
Roger Ramos <rodgerpaulo@hotmail.com>
2020-04-04 21:02:42 -0300

    Prevents unnecessary extra Navigation render

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { memo } from 'react'
 import { ROUTES } from 'routes'
 import { string } from 'prop-types'
 
@@ -22,4 +22,4 @@ Navigation.defaultProps = {
   category: ''
 }
 
-export default Navigation
+export default memo(Navigation)
