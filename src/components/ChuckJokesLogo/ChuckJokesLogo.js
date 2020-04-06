e5f16768f4e5fb611e1f6647d80093387b50d03c
Roger Ramos <rodgerpaulo@hotmail.com>
2020-04-06 00:47:26 -0300

    Update main logo

--- src/components/ChuckJokesLogo/ChuckJokesLogo.js
+++ src/components/ChuckJokesLogo/ChuckJokesLogo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import logo from 'assets/chuck-jokes-logo.png'
+import logo from 'assets/chuck-jokes-logo.svg'
 import LogoWrapper from './ChuckJokesLogo.style'
 
 export default props => <LogoWrapper {...props} src={logo} alt="Chuck Jokes Logo" />
