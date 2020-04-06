6edf2b6c11dea81f720c80acb70d17c6b1fd2a6b
Roger Ramos <rodgerpaulo@hotmail.com>
2020-04-06 00:29:08 -0300

    Finishes up Joke page

--- src/pages/Joke/Joke.js
+++ src/pages/Joke/Joke.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, { useEffect, useCallback } from 'react'
 import { useParams } from 'react-router'
+import { useSelector, useDispatch } from 'react-redux'
+import { jokeRequest } from 'store/reducers/joke'
+import Loading from 'components/Loading'
+import Error from 'components/Error'
 import Navigation from 'components/Navigation'
 import { Wrapper, JokeWrapper, Joke, Button } from './Joke.style'
 
 export default () => {
   const { category } = useParams()
-  const joke =
-    'Chuck Norris has volunteered to remain on earth after the Rapture; he will spend his time fighting the Anti-Christ.'
+  const dispatch = useDispatch()
+  const {
+    isFetching,
+    isSuccess,
+    isError,
+    data: { value }
+  } = useSelector(state => state.joke)
+
+  const handleJokeRequest = useCallback(() => dispatch(jokeRequest(category)), [dispatch, category])
+  useEffect(() => {
+    handleJokeRequest()
+  }, [handleJokeRequest])
 
   return (
-    <Wrapper>
-      <Navigation category={category} />
-      <JokeWrapper>
-        <Joke>{joke}</Joke>
-        <Button outline>Roll another</Button>
-      </JokeWrapper>
-    </Wrapper>
+    <>
+      {!isError ? (
+        <Wrapper>
+          {isFetching && <Loading data-testid="loader" />}
+          {!isFetching && isSuccess && (
+            <>
+              <Navigation category={category} />
+              <JokeWrapper>
+                <Joke>{value}</Joke>
+                <Button onClick={handleJokeRequest} outline>
+                  Roll another
+                </Button>
+              </JokeWrapper>
+            </>
+          )}
+        </Wrapper>
+      ) : (
+        <Error
+          buttonText="Try again"
+          errorMessage="Cannot get joke. Pleas try again later"
+          handleError={handleJokeRequest}
+        />
+      )}
+    </>
   )
 }
--- src/pages/Joke/Joke.spec.js
+++ src/pages/Joke/Joke.spec.js
@@ -1,24 +1,45 @@
 import React from 'react'
 import { MemoryRouter, Route, Switch } from 'react-router-dom'
 import { ROUTES } from 'routes'
-import { renderWithThemeAndProvider } from '__tests__/utils'
+import { renderWithThemeAndProvider, mockStore } from '__tests__/utils'
+import { initialState } from 'store/reducers/joke'
 import Joke from './Joke'
 
-const renderComponent = () =>
-  renderWithThemeAndProvider(
+const renderComponent = overWriteState => {
+  const store = mockStore({ joke: { ...initialState, ...overWriteState } })
+
+  return renderWithThemeAndProvider(
     <MemoryRouter initialEntries={[`${ROUTES.RAMDOM_JOKE}/science`]}>
       <Switch>
         <Route exact path={`${ROUTES.RAMDOM_JOKE}/:category?`}>
           <Joke />
         </Route>
       </Switch>
-    </MemoryRouter>
+    </MemoryRouter>,
+    store
   )
+}
 
 describe('Joke page', () => {
-  it('Should render Joke page', () => {
-    const { getByText } = renderComponent()
+  it('Should render loading state', () => {
+    const state = { isFetching: true }
+    const { getByTestId } = renderComponent(state)
 
-    expect(getByText(/science/)).toBeInTheDocument()
+    expect(getByTestId('loader')).toBeInTheDocument()
+  })
+
+  it('Should render Joke', async () => {
+    const joke = 'Joke content'
+    const state = { isFetching: false, isSuccess: true, data: { value: joke } }
+    const { getByText } = renderComponent(state)
+
+    expect(getByText(joke)).toBeInTheDocument()
+  })
+
+  it('Should render Error page', () => {
+    const state = { isError: true }
+    const { getByText } = renderComponent(state)
+
+    expect(getByText('Try again')).toBeInTheDocument()
   })
 })
