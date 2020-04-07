61c4f479714daa7aa635d6beade908561aae3ec6
Roger Ramos <rodgerpaulo@hotmail.com>
2020-04-07 00:49:24 -0300

    Reduce mobile margin to better space

--- src/components/Content/Content.style.js
+++ src/components/Content/Content.style.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components'
+import breakponts from 'config/media'
 
 export default styled.main`
   display: flex;
   flex-flow: column;
   align-items: center;
-  padding: 70px 42px;
+  padding: 70px 24px;
+
+  ${breakponts.tablet`
+    padding-left: 48px;
+    padding-right: 48px;
+  `}
 `
