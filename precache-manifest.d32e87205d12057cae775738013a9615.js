51709846779c3e8c95715cc7795c24e9c270302e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-01-26 22:02:05 +0000

    Deploy rogerramosme/chuck-jokes to github.com/rogerramosme/chuck-jokes.git:gh-pages

---
+++ precache-manifest.d32e87205d12057cae775738013a9615.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fb0ab9083045b45ac5d391fe7fff12c",
+    "url": "/chuck-jokes/index.html"
+  },
+  {
+    "revision": "10d59552d61937137d67",
+    "url": "/chuck-jokes/static/js/2.60234108.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/chuck-jokes/static/js/2.60234108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d715511847fcb1d29a18",
+    "url": "/chuck-jokes/static/js/3.622c8dd9.chunk.js"
+  },
+  {
+    "revision": "e28e9d7262e51a201f83",
+    "url": "/chuck-jokes/static/js/4.095a2e38.chunk.js"
+  },
+  {
+    "revision": "6e53a7e10f5bc5312dc4",
+    "url": "/chuck-jokes/static/js/5.d72cdaa6.chunk.js"
+  },
+  {
+    "revision": "c5771d6a7f02dbe06bbf",
+    "url": "/chuck-jokes/static/js/6.edd2f705.chunk.js"
+  },
+  {
+    "revision": "60283280242fdd80bf8d",
+    "url": "/chuck-jokes/static/js/main.c8840949.chunk.js"
+  },
+  {
+    "revision": "68fbaf4790d5138695c7",
+    "url": "/chuck-jokes/static/js/runtime-main.3df5494b.js"
+  },
+  {
+    "revision": "f9538dcae53be4e64834a864135bac49",
+    "url": "/chuck-jokes/static/media/chuck-jokes-logo.f9538dca.svg"
+  },
+  {
+    "revision": "2b883bfc906c8043d815ae57ab05b155",
+    "url": "/chuck-jokes/static/media/icon-back.2b883bfc.svg"
+  },
+  {
+    "revision": "0cd51c39ac7cf854be4f865a0753df97",
+    "url": "/chuck-jokes/static/media/icon-quote.0cd51c39.svg"
+  }
+]);
